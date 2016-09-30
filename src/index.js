484d621f244c611d0d3c707a2564bb2717e24c32
Eric Bond <eric@ericbond.net>
2016-09-30 15:12:22 -0400

    updated with new styles

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import Form from './inputForm.js';
+import Footer from './Footer.js';
 import './index.css';
 
 ReactDOM.render(
   <div>
   <App />
   <Form />
+  <Footer />
   </div>, 
   document.getElementById('root')
 );
--- src/inputForm.js
+++ src/inputForm.js
@@ -1,48 +1,71 @@
 import React, { Component } from 'react';
+import './inputForm.css'
 
 class Form extends Component {
     render() {
         return (
                  <div className="row">
-                     <div className="col-lg-4">
+                     <div className="col-lg-3">
                      </div>
-                     <div className="col-lg-4">
+                     <div className="col-lg-6">
              
-                        <form action="https://formspree.io/ericbond007@gmail.com" method="post" className="form-horizontal">
+                        <form action="https://formspree.io/ericbond007@gmail.com" method="post" className="form-inline">
                             <fieldset>
 
+                            <div id="topRow" className="col-md-12">
+
+                            <div className="col-md-3">
+                            </div>
 
 
-                            <div className="input-group input-group-lg form-group">
-                                <label className="control-label col-sm-2" htmlFor="name">Name:</label>
-                                <div className="col-sm-10">
-                                    <input id="name" type="text" className="form-control" placeholder="Name" name="Name" maxLength="255"/>
+                                <div className="col-md-3">
+                                    <div className="input-group input-group-lg form-group">
+                                    {/*<label className="control-label col-sm-2" htmlFor="name">Name:</label>*/}
+                                        <div >
+                                            <input id="name" type="text" className="form-control" placeholder="Name" name="Name" maxLength="255"/>
+                                        </div>
+                                    </div>
                                 </div>
-                             </div>
                              
-                             <div className="input-group input-group-lg form-group">
-                                <label className="control-label col-sm-2" htmlFor="email">Email:</label>
-                                <div className="col-sm-10">
-                                    <input id="email" className="form-control" type="email" placeholder="Email" name="email" maxLength="255"/>
+                                 <div className="col-md-3">
+                                     <div className="input-group input-group-lg form-group">
+                                     {/*<label className="control-label col-sm-2" htmlFor="email">Email:</label>*/}
+                                        <div className="col-md-6">
+                                            <input id="email" className="form-control" type="email" placeholder="Email" name="email" maxLength="255"/>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                            <div className="">
-                                <div className="">
-                                    <textarea className="form-control col-xs-12" rows="10" id="question" name="question" maxLength="9999"></textarea>
+                            <div className="col-md-3">
+                            </div>
+
+                            <div id="bottomRow" className="col-md-12">
+                                <div className="col-sm-1">
+                                </div>
+                                        <div className="col-md-9">
+                                            <textarea className="form-control fillColor" rows="10" cols="120" id="question" name="question" maxLength="9999"></textarea>
+                                        </div>
+                                <div className="col-sm-2">
                                 </div>
                             </div>
 
-                        
-                            <div className="input-group input-group-lg form-group input-lg">
-                                <div className="col-sm-10">
-                                    <input className="" type="submit" value="Ask Me!" />
+
+                            <div className="col-md-12">
+                                <div className="col-md-5">
+                                </div>
+                                    <div className="col-md-4">
+                                        <input id="askMe" className="btn btn-primary btn-lg" type="submit" value="Ask Me!" />
+                                    </div>
+                                <div className="col-md-3">
                                 </div>
                             </div>
-                            
+
                             </fieldset>
                         </form>
 
                       </div>
+                     <div className="col-lg-3">
+                     </div>
                  </div>
                 );
         }
