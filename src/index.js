a811e26108f6236b4fe006022808f9bc1fef284a
Eric Bond <eric@ericbond.net>
2016-09-30 10:26:35 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import Form from './inputForm.js';
+import './index.css';
+
+ReactDOM.render(
+  <div>
+  <App />
+  <Form />
+  </div>, 
+  document.getElementById('root')
+);
