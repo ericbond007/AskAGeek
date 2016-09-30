59666e5077e390b85f24e09017b8f7405959d019
Eric Bond <eric@ericbond.net>
2016-09-30 11:47:41 -0400

    Save local changes

--- src/inputForm.js
+++ src/inputForm.js
@@ -3,25 +3,49 @@ import React, { Component } from 'react';
 class Form extends Component {
     render() {
         return (
-         <div>
-         
-            <form className='QuestionForm' action="https://formspree.io/ericbond007@gmail.com" method="post">
-            <fieldset>
-                <label type="text" for="name">Name</label>
-                <input id="name" type="text" placeholder="Name" name="Name" />
-                
-                <label type="text" for="email">Email</label>
-                <input id="email" type="email" placeholder="Email" name="email" />
+                 <div className="row">
+                     <div className="col-lg-4">
+                     </div>
+                     <div className="col-lg-4">
+             
+                        <form action="https://formspree.io/ericbond007@gmail.com" method="post" className="form-horizontal">
+                            <fieldset>
 
-                <label type="text" for="question">Ask Me!</label>
-                <input id="question" type="textarea" name="question" />
-            </fieldset>
-            
-            <input type="submit" value="Ask Me!" />
-            </form>
-          </div>
-            );
+
+
+                            <div className="input-group input-group-lg form-group">
+                                <label className="control-label col-sm-2" for="name">Name:</label>
+                                <div className="col-sm-10">
+                                    <input id="name" type="text" className="form-control" placeholder="Name" name="Name" />
+                                </div>
+                             </div>
+                             
+                             <div className="input-group input-group-lg form-group">
+                                <label className="control-label col-sm-2" for="email">Email:</label>
+                                <div className="col-sm-10">
+                                    <input id="email" className="form-control" type="email" placeholder="Email" name="email" />
+                                </div>
+                            </div>
+                            <div className="">
+                                <div className="">
+                                    <textarea className="form-control col-xs-12" rows="10" id="question"></textarea>
+                                </div>
+                            </div>
+
+                        
+                            <div className="input-group input-group-lg form-group input-lg">
+                                <div className="col-sm-10">
+                                    <input className="" type="submit" value="Ask Me!" />
+                                </div>
+                            </div>
+                            
+                            </fieldset>
+                        </form>
+
+                      </div>
+                 </div>
+                );
+        }
     }
-}
 
-export default Form;
+    export default Form;
